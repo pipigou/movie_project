c3021fb0580d428618013363682f70c8823f3f93
cloudy <hot1931@gmail.com>
2018-08-19 22:18:15 +0800

    modification models

--- app/models.py
+++ app/models.py
@@ -1,10 +1,12 @@
-import datetime
+from datetime import datetime
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import pymysql
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:hotweather123@127.0.0.1:3306/movie"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:hotweather123@127.0.0.1:3306/movie"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 db = SQLAlchemy(app)
@@ -22,7 +24,7 @@ class User(db.Model):
     face = db.Column(db.String(255), unique=True)
     addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     uuid = db.Column(db.String(255), unique=True)
-    userlog = db.relationship('Userlog', backref='user')
+    userlogs = db.relationship('Userlog', backref='user')
     comments = db.relationship('Comment', backref='user')
     moviecols = db.relationship('Moviecol', backref='user')
 
@@ -48,7 +50,7 @@ class Tag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True)
     addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    movies = db.relationship("Comment", backref="tag")
+    movies = db.relationship("Movie", backref="tag")
 
     def __repr__(self):
         return "<Tag %r>" % self.name
@@ -58,7 +60,7 @@ class Tag(db.Model):
 class Movie(db.Model):
     __tablename__ = "movie"
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255))
+    title = db.Column(db.String(255), primary_key=True)
     url = db.Column(db.String(255), unique=True)
     info = db.Column(db.Text)
     logo = db.Column(db.String(255), unique=True)
@@ -81,7 +83,7 @@ class Movie(db.Model):
 class Preview(db.Model):
     __tablename__ = "preview"
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255))
+    title = db.Column(db.String(255), unique=True)
     logo = db.Column(db.String(255), unique=True)
     addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
@@ -113,6 +115,7 @@ class Moviecol(db.Model):
     def __repr__(self):
         return "<Moviecol %r>" % self.id
 
+
 # 权限
 class Auth(db.Model):
     __tablename__ = "auth"
@@ -124,6 +127,7 @@ class Auth(db.Model):
     def __repr__(self):
         return "<Auth %r>" % self.name
 
+
 # 角色
 class Role(db.Model):
     __tablename__ = "role"
@@ -131,6 +135,60 @@ class Role(db.Model):
     name = db.Column(db.String(100), unique=True)
     auths = db.Column(db.String(600))
     addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    admins = db.relationship("Admin", backref='role')
 
     def __repr__(self):
         return "<Role %r>" % self.name
+
+
+# 管理员
+class Admin(db.Model):
+    __tablename__ = "admin"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    pwd = db.Column(db.String(100), unique=True)
+    is_super = db.Column(db.SmallInteger)  # 是否为超级管理员，0为超级管理员
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    adminlogs = db.relationship("Adminlog", backref="admin")
+    Oplogs = db.relationship("Oplog", backref="admin")
+
+    def __repr__(self):
+        return "<Admin %r>" % self.name
+
+
+# 管理员登录日志
+class Adminlog(db.Model):
+    __tablename__ = "adminlog"
+    id = db.Column(db.Integer, primary_key=True)
+    admin_id = db.Column(db.Integer, db.ForeignKey('admin.id'))
+    ip = db.Column(db.String(16))
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "<Adminlog %r>" % self.id
+
+
+# 操作日志
+class Oplog(db.Model):
+    __tablename__ = "oplog"
+    id = db.Column(db.Integer, primary_key=True)
+    admin_id = db.Column(db.Integer, db.ForeignKey('admin.id'))
+    ip = db.Column(db.String(16))
+    reason = db.Column(db.String(600))
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "<Oplog %r>" % self.id
+
+
+if __name__ == "__main__":
+    db.create_all()
+    admin = Admin(
+        name='cloudy',
+        pwd=generate_password_hash("hotweather123"),
+        is_super=0,
+        role_id=1
+    )
+    db.session.add(admin)
+    db.session.commit()
